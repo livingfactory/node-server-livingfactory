2557a8bc144141681c3863978810f5934a7173d8
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-10-30 14:52:09 -0400

    Web server routing
    Learned to use web server routing in javascript

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const http = require('http');
 const fs = require('fs');
+const { isAbsolute } = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader = ('Content-Type', 'text/html');
+
+  let path = './';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
   fs.readFile('./index.html', (err, data) => {
     if (err) {
       console.error(err);
