023123a4208dd73641f5abccc7a6cd0a3dfb717d
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-10-30 14:29:10 -0400

    Writing to files
    Learned to write to text files in javascript

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
+const { appendFile } = require('fs');
 
-try {
-  const data = fs.readFileSync('hi.txt', 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(err);
-}
+const newContent = '\nThis is some more new text';
 
-console.log('Log from outside');
+appendFile('hi.txt', newContent, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Content written!');
+});
