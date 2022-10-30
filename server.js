5b5b1c84698ffa0e10bfc40530d2eb928d7f1840
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-10-30 14:21:30 -0400

    Global properties
    Worked with global properties filename dirname

--- server.js
+++ server.js
@@ -1,14 +1,2 @@
-const http = require('http');
-
-const HOSTNAME = process.env.HOSTNAME || 'localhost';
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World');
-});
-
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`server running at http://${HOSTNAME}:${PORT}`);
-});
+console.log(__filename);
+console.log(__dirname);
